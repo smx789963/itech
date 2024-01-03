001816874ba0fc8a606c5f12ed52caa938fe3952
Zichun Wang <2936888W@student.gla.ac.uk>
2024-01-03 15:16:46 +0000

    Initial commit

---
+++ rango/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 15:10:29 2024
+
+@author: hank
+"""
+
+from django.urls import path
+from rango import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    ]
