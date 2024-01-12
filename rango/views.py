0e9093071969c81539a7c8ddaa92967720c00b93
Zichun Wang <2936888W@student.gla.ac.uk>
2024-01-12 13:26:09 +0000

    some changes on database

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Return to main page</a>")
+    context_dict = {'yourname':"Zichun Wang"}
+    return render(request, 'rango/about.html', context=context_dict)
