12875d1431cca2a92786abdfa0640f0abc67531f
Zichun Wang <2936888W@student.gla.ac.uk>
2024-01-18 23:25:16 +0000

    changes on models.py, create and migrate the database

--- rango/models.py
+++ rango/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    def _str_(self):
+    def __str__(self):
         return self.name
 class Page(models.Model):
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    def _str_(self):
+    def __str__(self):
         return self.title
     
