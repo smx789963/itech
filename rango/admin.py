74bb555f5953213879aa3aca07284058bc9ef067
Zichun Wang <2936888W@student.gla.ac.uk>
2024-01-30 01:23:16 +0000

    chapter 5 finished

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from rango.models import Category, Page
 # Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category','title','url')
+admin.site.register(Category)
+admin.site.register(Page,PageAdmin)
